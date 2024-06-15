ef5284694413a8d77844ddf99ad457939e37ef12
KiKoS0 <22998716+KiKoS0@users.noreply.github.com>
2024-06-15 15:50:07 +0000

    feat: Stream from kmsg 🎉

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,37 @@
-use std::fs::OpenOptions;
-use std::io::Write;
-use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio::fs::File;
+use tokio::io::{self, AsyncBufReadExt, AsyncSeekExt, BufReader, SeekFrom};
 use tokio::process::Command;
 use tokio::time::{self, Duration};
 
-//TODO: Tone it down to only get the last X lines or figure out how to stream from the file.
-async fn execute() -> bool {
-    let mut cmd = Command::new("dmesg")
-        .stdout(std::process::Stdio::piped())
-        .kill_on_drop(true)
-        .spawn()
-        .unwrap();
+#[tokio::main]
+async fn main() {
+    loop {
+        process_kmsg().await.unwrap();
+    }
+}
 
-    let restart_msg_flag = "Restarted DSL connection";
-    let showtime_enter_flag = "vrx518_tc:ptm_showtime_enter";
+async fn process_kmsg() -> io::Result<()> {
+    let mut file = File::open("/dev/kmsg").await?;
+    file.seek(SeekFrom::End(0)).await?;
+
+    let reader = BufReader::new(file);
+    let mut lines = reader.lines();
+
     let showtime_exit_flag = "vrx518_tc:ptm_showtime_exit";
 
-    let stdout = cmd.stdout.as_mut().unwrap();
-    let mut stdout_reader = BufReader::new(stdout).lines();
-
-    let mut lines: Vec<String> = Vec::new();
-    while let Some(line) = stdout_reader.next_line().await.unwrap() {
-        lines.push(line);
-    }
-
-    for line in lines.iter().rev() {
-        if line.contains(restart_msg_flag) || line.contains(showtime_enter_flag) {
-            dbg_print_to_kernel_log("Skipping because of restart or showtime enter flag");
-            return false;
-        }
-
+    dbg_print("Listening...");
+    while let Some(line) = lines.next_line().await? {
         if line.contains(showtime_exit_flag) {
             restart_dsl_connection().await;
-            print_to_kernel_log(restart_msg_flag);
-            dbg_print_to_kernel_log("Sleeping for 60 seconds");
 
-            time::sleep(Duration::from_secs(60)).await;
-            return true;
+            dbg_print("Sleeping for 15 seconds, avoiding restart loop.");
+            time::sleep(Duration::from_secs(15)).await;
+
+            return Ok(());
         }
     }
 
-    cmd.wait().await.unwrap();
-
-    false
+    Ok(())
 }
 
 async fn restart_dsl_connection() {
@@ -84,7 +73,7 @@ async fn restart_dsl_connection() {
             let mut stdout_reader = BufReader::new(stdout).lines();
 
             while let Ok(Some(line)) = stdout_reader.next_line().await {
-                dbg_print_to_kernel_log(&line);
+                dbg_print(&line);
             }
         }
 
@@ -92,41 +81,6 @@ async fn restart_dsl_connection() {
     }
 }
 
-#[tokio::main]
-async fn main() {
-    loop {
-        if execute().await {
-            continue;
-        }
-
-        dbg_print_to_kernel_log("Sleeping for 10 second");
-        time::sleep(Duration::from_secs(10)).await;
-    }
-}
-
-fn dbg_print_to_kernel_log(msg: &str) {
-    do_print_to_kernel_log(msg, true)
-}
-
-fn print_to_kernel_log(msg: &str) {
-    do_print_to_kernel_log(msg, false)
-}
-
-fn do_print_to_kernel_log(msg: &str, debug: bool) {
-    if debug && !cfg!(debug_assertions) {
-        return;
-    }
-    let msg: String = format!("{}: {}", env!("CARGO_PKG_NAME"), msg);
-    println!("{}", msg);
-    let mut file = match OpenOptions::new().write(true).open("/dev/kmsg") {
-        Ok(file) => file,
-        Err(e) => {
-            eprintln!("Failed to open /dev/kmsg: {}", e);
-            return;
-        }
-    };
-
-    if let Err(e) = write!(file, "{}", msg) {
-        eprintln!("Failed to write to /dev/kmsg: {}", e);
-    }
+fn dbg_print(msg: &str) {
+    println!("{}: {}", env!("CARGO_PKG_NAME"), msg);
 }
